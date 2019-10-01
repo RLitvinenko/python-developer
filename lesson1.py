6e0bc7094e2efca474af5ac5769ab029ee2decfe
Roman <rl@jooble.com>
2019-10-01 15:58:16 +0300

    add lesson

---
+++ lesson1.py
@@ -0,0 +1,23 @@
+a = input('vvedite a')
+b = input('vvedite b')
+a = int(a)
+b = int(b)
+
+operator = input('input operatop')
+
+result = None
+if operator == '*':
+    result = a * b
+elif operator == '+':
+    result = a + b
+elif operator == '-':
+    result = a - b
+elif operator == '/':
+    result = a / b
+else:
+    print_('error')
+print("resultat", result)
+
+
+
+#print(f'result= {result}')
