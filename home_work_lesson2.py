26635874ca896291129c6fa0c80e16f037917433
rl <2makepchi@gmail.com>
2019-10-06 18:24:03 +0300

    ``

---
+++ home_work_lesson2.py
@@ -0,0 +1,19 @@
+# 1 задание
+#a = input('vvedite slovo')
+#b = input('vvedite slovo 2')
+#print (a, b)
+
+# 2 задание
+a = True
+b = True
+x = False
+a1 = int(a)
+b1 = int(b)
+x1 = int(x)
+print(a1, b1, x1)
+print (a and b and x)
+print (a and x and b)
+print (a and b)
+print (a and x)
+print (b and x)
+print (b and x and a)
--- lesson3.py
+++ lesson3.py
@@ -1,2 +1,10 @@
+x = int(input('x = '))
 if x > 5:
     print('x больше пяти')
+
+#x = int(input('x = '))
+#if x > 0:
+#    y = x ** 0.5
+#else:
+#    y = x ** 2
+#print(y)
