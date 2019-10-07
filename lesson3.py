0c624dfd3ba6ca064232aabcdba86e32373bc637
rl <2makepchi@gmail.com>
2019-10-07 18:56:13 +0300

    ``

--- lesson3.py
+++ lesson3.py
@@ -1,6 +1,6 @@
-x = int(input('x = '))
-if x > 5:
-    print('x больше пяти')
+#x = int(input('x = '))
+#if x > 5:
+#    print('x больше пяти')
 
 #x = int(input('x = '))
 #if x > 0:
@@ -8,3 +8,14 @@ if x > 5:
 #else:
 #    y = x ** 2
 #print(y)
+
+
+
+
+x = 5
+b = 3
+k = int(input())
+if k != x and k != b:
+    print('no morjo')
+else:
+    print('morjo')
